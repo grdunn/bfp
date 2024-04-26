054f3ce57ac3365a3f52c85578da5eeda07dc01f
Gregory Dunn <grdunn@gmail.com>
2024-04-26 19:00:30 -0400

    responsive react cta

--- src/Cta.tsx
+++ src/Cta.tsx
@@ -1,8 +1,13 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function Cta() {
-  // const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("20");
+  const url = `https://secure.actblue.com/donate/web-bfp-march2-2024?refcode=web_test&amount=`;
+
+  const handleRadio = (e: any) => {
+    setAmount(e.target.value);
+  };
 
   return (
     <>
@@ -13,27 +18,59 @@ function Cta() {
           </h3>
           <ul className="cta__list">
             <li className="cta__list-item">
-              <button className="cta__list-button" target="_blank">
+              <input
+                onChange={handleRadio}
+                value="10"
+                type="radio"
+                name="radio"
+                id="radio-10"
+                checked={amount === "10"}
+              />
+              <label htmlFor="radio-10" className="cta__list-button">
                 $10
-              </button>
+              </label>
             </li>
             <li className="cta__list-item">
-              <button className="cta__list-button" target="_blank">
+              <input
+                onChange={handleRadio}
+                value="20"
+                type="radio"
+                name="radio"
+                id="radio-20"
+                checked={amount === "20"}
+              />
+              <label htmlFor="radio-20" className="cta__list-button">
                 $20
-              </button>
+              </label>
             </li>
             <li className="cta__list-item">
-              <button className="cta__list-button" target="_blank">
+              <input
+                onChange={handleRadio}
+                value="30"
+                type="radio"
+                name="radio"
+                id="radio-30"
+                checked={amount === "30"}
+              />
+              <label htmlFor="radio-30" className="cta__list-button">
                 $30
-              </button>
+              </label>
             </li>
             <li className="cta__list-item">
-              <button className="cta__list-button" target="_blank">
+              <input
+                onChange={handleRadio}
+                value="40"
+                type="radio"
+                name="radio"
+                id="radio-40"
+                checked={amount === "40"}
+              />
+              <label htmlFor="radio-40" className="cta__list-button">
                 $40
-              </button>
+              </label>
             </li>
           </ul>
-          <a href="#" className="cta__button">
+          <a target="_blank" href={url + amount} className="cta__button">
             Donate
           </a>
         </div>
